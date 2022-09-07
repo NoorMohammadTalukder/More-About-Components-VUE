2e5cb56de2d956bb879d97f94e99ce6352bb90e2
YourName <noor11809027@gmail.com>
2022-09-07 17:01:20 +0600

    dynamic component

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,49 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
+    <button @click="show('active-goals')">Show Active</button>
+    <button @click="show('manage-goals')">Show Manage</button>
+    <!-- <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
-    ></user-info>
+    ></user-info> -->
+    <!-- <active-goals></active-goals>
+    <manage-goals></manage-goals> -->
+    <component :is='selectedComponent'></component>
   </div>
   
-  <course-goals>
+  <!-- <course-goals>
     <template #default="slotProps">
       <h2>{{slotProps.item}}</h2>
     </template>
-  </course-goals>
+  </course-goals> -->
 
 </template>
 
 <script>
   import TheHeader from './components/TheHeader.vue';
-  import BadgeList from './components/BadgeList.vue';
-  import UserInfo from './components/UserInfo.vue';
-  import CourseGoals from './components/CourseGoals.vue';
+  // import BadgeList from './components/BadgeList.vue';
+  // import UserInfo from './components/UserInfo.vue';
+  // import CourseGoals from './components/CourseGoals.vue'; 
+  import ActiveGoals from './components/ActiveGoals.vue'; 
+  import ManageGoals from './components/ManageGoals.vue'; 
   
   export default {
   components:{
     'the-header':TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals,
+    // BadgeList,
+    // UserInfo,
+    // CourseGoals,
+    ActiveGoals,
+    ManageGoals,
+
     
   },
   data() {
     return {
+      selectedComponent:"active-goals",
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
@@ -40,6 +51,13 @@
       },
     };
   },
+  methods:{
+    show(cmp){
+    this.selectedComponent=cmp;
+    // alert(cmp)
+  }
+  }
+ 
 };
 </script>
 
