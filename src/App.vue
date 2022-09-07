841a469ec4d052cb98afe51778352b8623637cbe
YourName <noor11809027@gmail.com>
2022-09-07 16:14:48 +0600

    scoped slot

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,28 @@
       :role="activeUser.role"
     ></user-info>
   </div>
+  
+  <course-goals>
+    <template #default="slotProps">
+      <h2>{{slotProps.item}}</h2>
+    </template>
+  </course-goals>
+
 </template>
 
 <script>
   import TheHeader from './components/TheHeader.vue';
   import BadgeList from './components/BadgeList.vue';
   import UserInfo from './components/UserInfo.vue';
+  import CourseGoals from './components/CourseGoals.vue';
   
   export default {
   components:{
     'the-header':TheHeader,
     BadgeList,
     UserInfo,
+    CourseGoals,
+    
   },
   data() {
     return {
---
+++ src/components/CourseGoals.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+        <li v-for="goal in goals" :key="goal">
+            <!-- {{goal}} -->
+        <slot :item="goal"></slot>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                goals:["Goal no 1","Goal no 2"]
+            }
+        }
+    }
+</script>
